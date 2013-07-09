c2d2d0e31865545b1a74ac388abf6434b3a4c74c
yuvalw <yuvalw@gmail.com>
2013-07-10 00:55:29 +0300

    added index.html and read it

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
-var express = require('express');
+var express = require('express'),
+    fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(readIndexHtml());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+function readIndexHtml(){
+    var file = fs.readFileSync('index.html', {encoding:'ascii'});
+    return file;
+}
