7d31ce2090fcbe82e5be009e9d4197e99b56f37b
yuvalw <yuvalw@gmail.com>
2013-08-07 16:04:02 +0300

    [=]

--- web.js
+++ web.js
@@ -4,15 +4,15 @@ var express = require('express'),
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(readIndexHtml());
+	response.send(readIndexHtml());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
 
-function readIndexHtml(){
-    var buffer = fs.readFileSync('index.html');
-    return buffer.toString();
+function readIndexHtml() {
+	var buffer = fs.readFileSync('index.html');
+	return buffer.toString();
 }
