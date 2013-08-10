23794cc87a93d41289ea97387ef2985127a7f8d6
yuvalw <yuvalw@gmail.com>
2013-08-11 01:31:36 +0300

    [+] add Social buttons

--- web.js
+++ web.js
@@ -3,16 +3,9 @@ var express = require('express'),
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-	response.send(readIndexHtml());
-});
+app.use("/", express.static(__dirname));
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
 	console.log("Listening on " + port);
 });
-
-function readIndexHtml() {
-	var buffer = fs.readFileSync('index.html');
-	return buffer.toString();
-}
